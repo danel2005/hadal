7a2026a4283475ea66f43289d8fed22dab0bfa72
danel2005 <dcoolmineduck@gmail.com>
2024-07-06 21:54:25 +0300

    Fixes

--- script.js
+++ script.js
@@ -26,13 +26,14 @@ function changeName(element) {
     var nameBoxes = document.getElementsByClassName('name-box');
     var all_danel = true;
     for (var i = 0; i < nameBoxes.length; i++) {
-        if(nameBoxes[i].innerText != 'דנאל')
-        {
+        if(nameBoxes[i].innerText != 'דנאל') {
             all_danel = false;
-        } // This will log each element with the class 'name-box'
+            break;
+        }
     }
     if (all_danel) {
-        window.location.href = "final_win.html"; // Redirect to win page if answer is correct
+        // document.getElementById('victorySound').play(); // Play the audio when all names are "Danel"
+        window.location.href = "final_win.html"; // Optional: Redirect to a final win page
     } 
 }
 
